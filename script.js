23b7fc817b31cf10cb2eda8081b4edb199f11470
Bhoomi678 <bhoomilakhwani39@gmail.com>
2024-10-05 22:19:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add a scroll event listener to the window
+window.addEventListener('scroll', function() 
+{
+  const navbar = document.getElementById('navbar');
+  
+  // Check if the page is scrolled more than 50px
+  if (window.scrollY > 50) 
+  {
+    navbar.classList.add('scrolled');
+  } 
+  else 
+  {
+    navbar.classList.remove('scrolled');
+  }
+}
+);
